9c89b9719973156ac7c1ca101fc28f8ee94456b3
GiovanaMaia <giovanamaia851@gmail.com>
2024-11-28 21:35:52 -0300

    Program
    Codigo do canvas responsavel pelos textos na tela (cena que aparece quando o jogador escolhe "personagem " no menu inicial)

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class canvaScript : MonoBehaviour
+{
+    public Text infoText;
+
+    public void Start()
+    {
+
+        Personagem personagem = new Personagem("Ella Aigam", 17, "3°");
+
+        // Salvando informações no PlayerPrefs
+        PlayerPrefs.SetString("Nome", personagem.Nome);
+        PlayerPrefs.SetInt("Idade", personagem.Idade);
+        PlayerPrefs.SetString("Posicao", personagem.Posicao);
+        PlayerPrefs.Save();
+
+        // Recuperando as informações do PlayerPrefs
+        string nome = PlayerPrefs.GetString("Nome", "Desconhecido");
+        int idade = PlayerPrefs.GetInt("Idade", 0);
+        string posicao = PlayerPrefs.GetString("Posicao", "N");
+
+        // Exibindo as informações no Text UI
+        infoText.text = $"Nome: {nome}\nIdade: {idade} anos\nPosição: {posicao} herdeira do trono de Aigam";
+    }
+    public void LoadScenes(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
