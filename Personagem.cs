d50509233e591fbf6bfbd9e59e0126deb30f6c91
GiovanaMaia <giovanamaia851@gmail.com>
2024-11-28 21:37:31 -0300

    Personagem
    Código da classe que é implementada no outro script

---
+++ Personagem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Personagem : MonoBehaviour
+{
+
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Posicao { get; set; }
+
+    // Construtor para inicializar o personagem
+    public Personagem(string nome, int idade, string posicao)
+    {
+        Nome = nome;
+        Idade = idade;
+        Posicao = posicao;
+    }
+
+    // Método para obter as informações do personagem em formato de string
+    public string ObterInformacoes()
+    {
+        return "Nome: " + Nome + "\nIdade: " + Idade + " anos" + "\nPosição: " + Posicao + " herdeira do trono de Aigam";
+    }
+
+
+}
+
+
